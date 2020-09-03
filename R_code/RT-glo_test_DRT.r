06c06250eb4634fd6843741080dc60a67b4f5fe4
Darren Tyson - Office <darren.tyson@vanderbilt.edu>
2020-09-03 18:49:09 -0500

    Adding R code for visualizing raw luminescence data (H841+TAK901)

--- R_code/RT-glo_test_DRT.r
+++ R_code/RT-glo_test_DRT.r
@@ -15,3 +15,18 @@ for(well in unique(tall$well)) lines(counts ~ time, data=tall[tall$well==well,])
 
 m <- lm(counts ~ well * time, data=tall)
 
+
+d2 <- read.table('../data/cleaned_dynamic_data.txt', as.is=TRUE, header=TRUE)
+
+library(ggplot2)
+
+ggplot(d2, aes(tdata, y = log2(value), color = variable)) + 
+    geom_line(aes(y = log2(DMSO_mean), col = "DMSO_mean")) + 
+    geom_line(aes(y = log2(X10uM_mean), col = "X10uM_mean")) +
+    geom_line(aes(y = log2(cleaned_2.5uM_mean), col = "cleaned_2.5uM_mean")) +
+    geom_line(aes(y = log2(cleaned_625nM_mean), col = "cleaned_625nM_mean")) +
+    geom_line(aes(y = log2(cleaned_156.25nM_mean), col = "cleaned_156.25nM_mean")) +
+    geom_line(aes(y = log2(cleaned_39nM_mean), col = "cleaned_39nM_mean")) +
+    geom_line(aes(y = log2(cleaned_9.76nM_mean), col = "cleaned_9.76nM_mean")) +
+    geom_line(aes(y = log2(cleaned_2.44nM_mean), col = "cleaned_2.44nM_mean"))
+
