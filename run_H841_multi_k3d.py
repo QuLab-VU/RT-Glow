a6c0e2f7d73d5918aec6e592d77ac2df9355921d
martinapr <39745889+martinapr@users.noreply.github.com>
2020-10-02 18:07:03 -0500

    Update run_H841_multi_k3d.py

--- run_H841_multi_k3d.py
+++ run_H841_multi_k3d.py
@@ -1,5 +1,5 @@
 from simplepso.pso import PSO
-from model_H841 import model
+from model_H841_k3d import model
 import os
 import pandas as pd
 #os.environ['BNGPATH'] = r'C:\Users\pinojc\Desktop\BioNetGen-2.5.1'
