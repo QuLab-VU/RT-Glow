82d6559d641da157d4c76409b4f9964d69bc5e2f
Leonard A. Harris <lh64@cornell.edu>
2021-05-22 04:14:00 -0500

    Resolving conflict in run_H841_multi_k3d.py

--- run_H841_multi_k3d.py
+++ run_H841_multi_k3d.py
@@ -1,30 +1,23 @@
+# from model_H841_k3d import model
+from model_H841 import model
 from simplepso.pso import PSO
-from model_H841_k3d import model
-import os
 import pandas as pd
-#os.environ['BNGPATH'] = r'C:\Users\pinojc\Desktop\BioNetGen-2.5.1'
 import numpy as np
-from pysb.simulator import ScipyOdeSimulator, BngSimulator
-from math import sqrt
+import matplotlib.pyplot as plt
+from pysb.simulator import ScipyOdeSimulator #, BngSimulator
 from scipy.constants import N_A
-from numpy.core.numeric import inf
-from pysb.bng import generate_equations
 import sys
 import csv
-from pysb.pathfinder import set_path
-#from pysb.generator.bng import BngGenerator
-
-from scipy import stats
-import matplotlib.pyplot as plt
-import math as m
+import re
+import os
+from get_data_LAH import get_data
 
 #=======================
 #=====import data=======
 #=======================
-
-static_initial_cells= []
+'''
+static_initial_cells = []
 static_measured_lum = []
-
 ct = 0
 with open('data/cleaned_static_data.txt', newline='') as csvfile_s:
     full_data_s = csv.reader(csvfile_s, delimiter=' ')
@@ -33,11 +26,37 @@ with open('data/cleaned_static_data.txt', newline='') as csvfile_s:
             static_initial_cells.append(float(row[0]))
             static_measured_lum.append(float(row[1]))
         ct = ct+1
-#print(static_initial_cells)
-#print(static_measured_lum)
+'''
+# exp_data = pd.read_csv('data/cleaned_dynamic_data.txt', delimiter='\s')
 
-exp_data = pd.read_csv('data/cleaned_dynamic_data.txt', delimiter='\s')
 
+'''
+def get_drug_conc_and_column_names():
+    # get drug concentrations from column names
+    conc = []
+    names = []
+    for col in exp_data.columns:
+        match = re.search("DMSO_", col)
+        if match:
+            conc.append(0)
+            names.append(col)
+        else:
+            match = re.search("(\d+\.*\d*)(\w+)_", col)
+            if match:
+                units = match.group(2)
+                if units == 'uM':
+                    scale = 1e-6
+                elif units == 'nM':
+                    scale = 1e-9
+                else:
+                    print('Error: Concentration units \'%s\' not recognized.' % units)
+                    quit()
+                conc.append(scale*float(match.group(1)))
+                names.append(col)
+    # print(drug_conc)
+    # quit()
+    return conc, names       
+'''
 #=======================
 #=====prepare PSO=======
 #=======================
@@ -51,164 +70,150 @@ solver = ScipyOdeSimulator(
 )
 
 # solver = BngSimulator(model, verbose=False, method='ode')
-generate_equations(model)
-spec_list = [str(x) for x in model.species]
-init_cell = spec_list.index("Cells(NADPHoxidoreductase1=None, drug='undrugged')")
+# generate_equations(model)
+spec_list = [str(sp) for sp in model.species]
+cell_idx = spec_list.index("Cells(NADPHoxidoreductase1=None, drug='undrugged')")
 drug_idx = spec_list.index('Drug()')
 
 param_names = np.array([p.name for p in model.parameters])
 param_values = np.array([p.value for p in model.parameters])
 
+k5_val = param_values[list(param_names).index('K5')]
+k6_val = param_values[list(param_names).index('K6')]
+
 # parameters: k3(T), k5(F), k6(F), km(T), kcat(T), cc(F), kon(T), koff(T), k3d(T), k5d(T), k6d(T), gain(F), cells0(F), nanoluc0(F), prosub0(F), drug0(F)
 # rate mask: k3(T), km(T), kcat(T), kon(T), koff(T), k3d(T), k5d(T), k6d(T)
-rate_mask = np.array([True, False, False, True, True, False, True, True, True, True, True, False, False, False, False, False])
+free_params = ['K3', 'Km', 'Kcat', 'kon', 'koff', 'K3d', 'K5d', 'K6d']
+rate_mask = np.array([p.name in free_params for p in model.parameters])
+# rate_mask_old = np.array([True, False, False, True, True, False, True, True, True, True, True, False, False, False, False, False])
 
-original_values = np.array([p.value for p in model.parameters])
-k5 = original_values[1]
-k6 = original_values[2]
-
-k5d_idx = np.where(param_names[rate_mask] == 'K5d')[0]
-k6d_idx = np.where(param_names[rate_mask] == 'K6d')[0]
+k5d_idx = list(param_names[rate_mask]).index('K5d')
+k6d_idx = list(param_names[rate_mask]).index('K6d')
 
 # We search in log10 space for the parameters
-starting_position = np.log10(param_values[rate_mask])
+starting_params = np.log10(param_values[rate_mask])
 
+def presim(param_values, cell_init, t_max, n_pts):
 
-def presim(param_values, cellcount, drug_seed, endpoints, refinements):
-    # check, if parameter set could not find a good fit for the initial concentration:
-
-    percent_range = drug_seed * 0.1
-
-    #def check_index(stop, spacing):
-    tspan = np.linspace(0, endpoints, int(refinements))
-    traj_init = solver.run(
-
-        param_values=param_values,
-        initials={
-            model.species[init_cell]: cellcount,
-            model.species[drug_idx]: 0
-        },
-        tspan=tspan,
-    )
-    lum = traj_init.expressions['Luminescence']
-
-
-
-    # find equilibrium
-    diff = (np.roll(lum,-1)-lum)/(tspan[1]-tspan[0])
-    diff = abs(diff/abs(lum))
-
-    diff_loc = diff < 2
-    diff_loc_1 = np.where(diff_loc)[0]
-
-    '''
-    print('diff value: ', diff[diff_loc][0], diff_loc_1[0])
-
-    plt.plot(lum, 'x')
-    plt.title('luminescence')
-    plt.show()
-    plt.plot(diff, 'x')
-    plt.title('diff')
-    plt.show()
-    '''
-
-    #lum_locate = abs(lum - drug_seed) < percent_range
-    #locations = lum[lum_locate]
-
-    #print()
-    if len(lum[diff_loc]) == 0:
-        return [], np.inf
-    else:
-        return traj_init.species[diff_loc][0], abs(lum[diff_loc][0] - drug_seed)/drug_seed
-
-
-'''
-        if len(locations) > 0:
-            return traj_init.species[lum_locate][0]
-        else:
-            return None
+    tspan = np.linspace(0,t_max,n_pts)
         
+    x = solver.run(tspan=tspan, param_values=param_values, 
+        initials={
+            model.species[cell_idx]: cell_init,
+            model.species[drug_idx]: 0}
+        )
+    
+    # find equilibrium time point
+    lum = x.expressions['Luminescence']
+    delta_t = tspan[1]-tspan[0]
+    slope = (np.roll(lum,-1)[:-1]-lum[:-1])/delta_t
+    # slope starts out large, declines, and then increases again
+    # define equilibrium as first point at which slope begins increasing
+    for i in range(len(slope)-1):
+        if slope[i+1] > slope[i]:
+            break
+    equil_idx = i
+    t_equil = tspan[equil_idx]
+    tspan_equil = tspan[:equil_idx+1] - t_equil
+    
+    # make sure equilibrium has been reached
+    # if not, return a large cost function value
+    equil_reached = True
+    if (i == len(slope)-2):
+        equil_reached = False
 
-    # value = check_index(5, 1e7)
-    # if value is not None:
-    #     return value
-    for ep in endpoints:
-        for r in refinements:
-            value = check_index(ep, r)
-            if value is not None:
-                return value
-    return []
-'''
-
+    return equil_reached, x.species[equil_idx]
 
 # --------function for the static cell data---------------
 
 def obj_static(position):
-    param_values[rate_mask] = 10 ** position.copy()
+    
+    param_values[rate_mask] = 10**position.copy()
 
     lum_ct = []
     tspan = np.linspace(0, 0.5, 10)
     for i in static_initial_cells:
         traj = solver.run(param_values=param_values,
-                          initials={model.species[init_cell]: i,
-                                    model.species[drug_idx]: 0}, tspan=tspan)
+                          initials={model.species[cell_idx]: i,
+                                    model.species[drug_idx]: 0}, 
+                          tspan=tspan)
         lum_ct.append(traj.expressions['Luminescence'][-1])
 
     return np.sum((np.array(static_measured_lum) - np.array(lum_ct)) ** 2) / (
         np.sum(np.array(static_measured_lum) ** 2))
 
-
 # --------functions for the dynamic cell data---------------
 
-def obj_dynamic(position, cellcount, drug_amount, drug_data, tol):
-    param_values[rate_mask] = 10 ** position.copy()
+def obj_dynamic(position, cellcount, drug_amount, t_data, drug_data, tol):
+
     verbose = False
     if verbose:
         print('compute initials for drug {} using a lum start of {}'
               ''.format(drug_amount, drug_data[0]))
 
-    initials, fit = presim(
-        param_values, cellcount, drug_data[0],
-        1, 1e4
-        #[1, 0.5, 0.1, 0.01, 0.001, 0.0001, 1e-5, 1e-6, 1e-7, 1e-8],
-        #[1e4, 1e5, 1e6]
-    )
-
-
-    if len(initials) == 0:
-        if verbose:
-            print("NOOOOOOOOO")
+    param_values[rate_mask] = 10**position.copy()
+    
+    # run a pre-simulation w/ no drug
+    equil_reached, initials = presim(param_values, cellcount, 1, 10001)
+    if not equil_reached:
         return 1e8
-
+    
+    # now add the drug and run another simulation
     initials[drug_idx] = drug_amount
-
-
     traj = solver.run(
         param_values=param_values,
         initials=initials,
-        tspan=exp_data['tdata'].values
+        tspan=t_data #exp_data['tdata'].values
     )
-    lum_traj = traj.expressions['Luminescence']
-    error = np.sum(((drug_data - lum_traj)/(drug_data)) ** 2)
-    return error
-
+    lum = traj.expressions['Luminescence']
+    sse = np.sum(((drug_data - lum)/drug_data) ** 2)
+    
+    return sse
 
 # ----- PSO -----
 
 def costfunction(parameter):
+    
+    # k5 must be larger than k5d (division rate)
+    if k5_val < 10**parameter[k5d_idx]:
+        return 1e8
+    # k6 must be smaller than k6d (death rate)
+    if k6_val > 10**parameter[k6d_idx]:
+        return 1e8
+    
     tol = 1e-5
-    # k5 needs to keep larger than k5d k3(T), km(T), kcat(T), kon(T), koff(T),
-    # k5d(T), k6d(T)
-    if k5 < parameter[k5d_idx]:
-        return 1e8
-    # k6 needs to keep smaller than k6d
-    if k6 > parameter[k6d_idx]:
-        return 1e8
-    # error_static = 1*obj_static(parameter)
-    # print('error static: ', error_static)
+    
+    # if 'drug_conc' is not a list, make it a list
+    try:
+        iter(drug_conc)
+    except TypeError:
+        conc = [drug_conc]
+    else:
+        conc = drug_conc
+    
+    # if 'data_column' is not a list, make it a list
+    try:
+        iter(data_column)
+    except TypeError:
+        col = [data_column]
+    else:
+        col = data_column
+        
+    # error check
+    if len(conc) != len(col):
+        print('Error: length of \'drug_conc\' (%d) and \'data_column\' (%d)' + 
+              'must be equal' % (len(conc), len(col)))
+        quit()
+    
+    err = [None]*len(conc)
+    for j in range(len(conc)):
+        err[j] = obj_dynamic(parameter, 300, conc[j]*N_A*80e-6, 
+                             exp_data['tdata_%s' % col[j]],
+                             exp_data['lum_%s_mean' % col[j]], tol)
 
-    error0 = obj_dynamic(parameter, 300, 0.15e-9*N_A*80e-6, exp_data['cleaned_0.15nM_mean'], tol)
-
+#     error0 = obj_dynamic(parameter, 300, 0.15e-9*N_A*80e-6, 
+#                          exp_data['cleaned_0.15nM_mean'], tol)
+    
     # 'DMSO_mean', '10uM_mean', 'cleaned_2.5uM_mean',
     # 'cleaned_625nM_mean', 'cleaned_156.25nM_mean', 'cleaned_39nM_mean',
     # 'cleaned_9.76nM_mean', 'cleaned_2.44nM_mean', 'cleaned_0.61nM_mean',
@@ -244,7 +249,9 @@ def costfunction(parameter):
 
     # total_err = error_static +error0+error1+error2+error3+error4+error5+error6+error7+error8+error9
     # total_err = error3+error4+error5+error6+error7+error8+error9
-    total_err = error0
+#     total_err = error0
+    total_err = sum(err)
+    
     if np.isnan(total_err):
         return 1e8
     else:
@@ -254,38 +261,44 @@ def convert_to_flat_array(optimizer, model):
     # convert to array
     history = np.array(optimizer.all_history)
     fitness = np.array(optimizer.all_fitness)
-    # create name masks
-    rate_params = model.parameters_rules()
-    rate_mask = np.array([p in rate_params for p in model.parameters])
-    param_values = np.array([p.value for p in model.parameters])
-    param_names = np.array([p.name for p in model.parameters])
+#     print(fitness)
+#     print(fitness.shape)
+#     print(history.shape)
+#     quit()
     col_names = list(param_names[rate_mask]) + ['fitness']
+#     print(col_names)
+
     # convert to pandas dataframe
     all_df = []
     for i in range(history.shape[1]):
-        pos = history[:, 0]
-        fit_value = fitness[:, 0]
+        pos = history[:, i]
+        fit_value = fitness[:, i]
+#         print(fit_value)
         stacked = np.column_stack([pos, fit_value])
         new_df = pd.DataFrame(stacked, columns=col_names)
         new_df['particle'] = i
         all_df.append(new_df)
+    
     return pd.concat(all_df)
 
-
-def run_pso(run, iterations, bd):
-    pso = PSO(save_sampled=False, verbose=True, shrink_steps=False)
+def run_pso(run, iterations, bd, outdir = '', suffix=''):
+    
+    pso = PSO(save_sampled=True, verbose=True, shrink_steps=False)
+    
     #pso.set_cost_function(costfunction)
-    pso.set_start_position(starting_position)
+    pso.set_start_position(starting_params)
     pso.set_bounds(bd)
-    pso.set_speed(-.1, .1)
+    pso.set_speed(-0.1, 0.1)
 
     pso.run(num_particles=20, num_iterations=iterations, stop_threshold=1e-5,
-            cost_function=costfunction, max_iter_no_improv=50,
+            cost_function=costfunction, max_iter_no_improv=500,
             num_processors=20, save_samples=True)
-
+    
     param_sets = convert_to_flat_array(pso, model)
+    
     #print(param_sets)
-    param_sets.to_csv('run'+run+'.csv')
+    outfile = os.path.join(outdir, 'run%d%s.csv' % (run,suffix))
+    param_sets.to_csv(outfile, index_label='iter')
     # print('best pos: ', pso.best.pos)
     #hist_flat = [10**item for sublist in pso.all_history for item in sublist]
     #hist_flat = pso.all_history.ravel()
@@ -300,7 +313,6 @@ def run_pso(run, iterations, bd):
     #np.savetxt("H841_params_" + str(run) + ".txt", hist_flat, delimiter=",")
     #np.savetxt("H841_fit_" + str(run) + ".txt", fit_flat, delimiter=",")
 
-
 def main():
     
     arg_lst = []
@@ -309,20 +321,41 @@ def main():
 
     if len(arg_lst) == 3:
         run = arg_lst[0]
-        data_file = arg_lst[1]
         iterations = int(arg_lst[1])
         bounds = float(arg_lst[2])
     else:
         run = 0
         iterations = 100
         bounds = 1
-    run_pso(run=run, iterations=iterations, bd=bounds)
-    '''
-    run = str(1)
-    iterations = 100
-    bounds = 4
-    run_pso(run = run, iterations = iterations, bd = bounds)
-    '''
+    
+    FILENAME = 'data/H1048_DMS53_Combined_Lum_and_cell_counts.csv' #'data/H841_Lum_only.csv'
+    CELL_LINE = 'NCIH1048' #'H841'
+    DRUG_NAME = 'TAK-901'
+    
+    global exp_data 
+    exp_data = get_data(FILENAME, CELL_LINE, DRUG_NAME)
+    
+#     drug_conc_all, col_names_all = get_drug_conc_and_column_names()
+    drug_conc_all = [0, 10000, 2500, 625, 156, 39.1, 9.77, 2.44, 0.61, 0.15] # nM
+
+#     drug_conc_samples = [[drug] for drug in drug_conc_all]    
+#     col_names_samples = [[col] for col in col_names_all] 
+
+    drug_conc_samples = [[conc*1e-9] for conc in drug_conc_all] 
+    col_names_samples = [[str(conc)+'nM'] for conc in drug_conc_all] 
+    
+    # make output directory
+    outdir = '%s_%s' % (CELL_LINE, DRUG_NAME) #'H841_TAK901' #'H1048_TAK901'
+    os.makedirs(outdir, exist_ok=True)
+    
+    global drug_conc
+    global data_column
+    for i in range(len(drug_conc_samples)):
+        drug_conc = drug_conc_samples[i] 
+        data_column = col_names_samples[i] 
+        print(drug_conc, data_column)
+        run_pso(run=i, iterations=iterations, bd=bounds, 
+                outdir = outdir, suffix='_no_k3d_iter_%d' % iterations)
 
 if '__main__' == __name__:
     main()
